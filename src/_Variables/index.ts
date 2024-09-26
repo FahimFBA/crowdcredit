1aebd268ffa309b4da7fa975d4fb1787c7d1c409
AbdulHaqueRahiD <149531767+AbdulHaqueRahiD@users.noreply.github.com>
2024-09-26 16:41:15 +0000

    add var

---
+++ src/_Variables/index.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_BASE_URL = import.meta.env.VITE_APP_SUPABASE_URL;
+export const SUPABASE_ANON_KEY = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
+export const containerDefinition = "container mx-auto max-w-5xl";
