c559c96b6b1be94c5a500113252d4a298a45dd25
FahimFBA <fahimbinamin@gmail.com>
2024-10-04 10:46:24 +0600

    Create useControlledState.ts

---
+++ src/_Hooks/useControlledState.ts
@@ -0,0 +1,26 @@
+import { useCallback, useEffect, useState } from "react";
+import { IUseControlledStateProps } from "./_Types";
+
+export const useControlledState = ({
+  controlledState,
+  onStateChange,
+}: IUseControlledStateProps) => {
+  const [localState, setLocalState] = useState<boolean>(
+    controlledState ?? false,
+  );
+
+  useEffect(() => {
+    if (controlledState !== undefined) {
+      setLocalState(controlledState);
+    }
+  }, [controlledState]);
+
+  const handleStateChange = useCallback(
+    (newState: boolean) => {
+      onStateChange ? onStateChange(newState) : setLocalState(newState);
+    },
+    [onStateChange],
+  );
+
+  return [localState, handleStateChange] as const;
+};
