4c9ac123528d870ca8006a6602f891e70a67081d
FahimFBA <fahimbinamin@gmail.com>
2024-10-05 23:51:51 +0600

    Update Home.tsx
    update cover image

--- src/pages/GeneralPages/Home.tsx
+++ src/pages/GeneralPages/Home.tsx
@@ -17,7 +17,7 @@ export const Home = withTemplate(() => {
       <div
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: `url(https://images.unsplash.com/photo-1726137570000-70ae29f0ba01?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
+          backgroundImage: `url(https://images.unsplash.com/photo-1622186477895-f2af6a0f5a97?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
         }}
       />
 
