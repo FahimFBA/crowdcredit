d507d46675c96f7da6dd719e4866a84db146ee32
Israt Jahan Khan <isratjahankhanijk@gmail.com>
2024-10-05 21:15:15 +0600

    update: crowdfunding sheet

--- src/components/Sheets/CrowdFundingSheet.tsx
+++ src/components/Sheets/CrowdFundingSheet.tsx
@@ -1,16 +1,6 @@
-import {
-  Button,
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "../ui";
 import { InputField } from "../Form/InputField";
-import { ICrowdFundingSheet } from "@/types/interface";
+import { IBaseSheetComponent, ICrowdFundingPost } from "@/types/interface";
+import { BaseSheetComponent } from "./BaseSheetComponent";
 
 export const CrowdFundingSheet = ({
   title,
@@ -23,72 +13,59 @@ export const CrowdFundingSheet = ({
   setSheetState,
   sheetState,
   buttonText,
-}: ICrowdFundingSheet) => {
+}: Omit<IBaseSheetComponent, "children"> & {
+  data: Partial<ICrowdFundingPost>;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) => {
   return (
-    <Sheet open={sheetState} onOpenChange={setSheetState}>
-      <SheetTrigger>{trigger}</SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>{title}</SheetTitle>
-          <SheetDescription>{description}</SheetDescription>
-        </SheetHeader>
-        <form onSubmit={handleSubmit}>
-          <div className="my-4 flex flex-col gap-3">
-            <InputField
-              id="title"
-              placeholder="Write a title for your crowd funding project"
-              type="text"
-              label="Crowd Funding Title"
-              value={data?.title}
-              onChange={handleChange}
-              required
-            />
-            <InputField
-              id="business_name"
-              placeholder="Write business name"
-              type="text"
-              label="Business Name"
-              value={data?.business_name}
-              onChange={handleChange}
-              required
-            />
-            <InputField
-              id="business_description"
-              placeholder="Write business description"
-              type="text"
-              label="Business Description"
-              value={data?.business_description}
-              onChange={handleChange}
-              required
-            />
-            <InputField
-              id="target_amount"
-              placeholder="Write target amount"
-              type="number"
-              label="Target Amount"
-              value={data?.target_amount}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <SheetFooter>
-            <SheetClose>
-              <Button variant="link" size="sm" type="button">
-                cancel
-              </Button>
-            </SheetClose>
-            {isLoading ? (
-              <Button variant="secondary" disabled size="sm" type="submit">
-                Creating....
-              </Button>
-            ) : (
-              <Button variant="default" size="sm" type="submit">
-                {buttonText}
-              </Button>
-            )}
-          </SheetFooter>
-        </form>
-      </SheetContent>
-    </Sheet>
+    <BaseSheetComponent
+      title={title}
+      description={description}
+      trigger={trigger}
+      buttonText={buttonText}
+      handleSubmit={handleSubmit}
+      isLoading={isLoading as boolean}
+      sheetState={sheetState}
+      setSheetState={setSheetState}
+    >
+      <div className="my-4 flex flex-col gap-3">
+        <InputField
+          id="title"
+          placeholder="Write a title for your crowd funding project"
+          type="text"
+          label="Crowd Funding Title"
+          value={data?.title}
+          onChange={handleChange}
+          required
+        />
+        <InputField
+          id="business_name"
+          placeholder="Write business name"
+          type="text"
+          label="Business Name"
+          value={data?.business_name}
+          onChange={handleChange}
+          required
+        />
+        <InputField
+          id="business_description"
+          placeholder="Write business description"
+          type="text"
+          label="Business Description"
+          value={data?.business_description}
+          onChange={handleChange}
+          required
+        />
+        <InputField
+          id="target_amount"
+          placeholder="Write target amount"
+          type="number"
+          label="Target Amount"
+          value={data?.target_amount}
+          onChange={handleChange}
+          required
+        />
+      </div>
+    </BaseSheetComponent>
   );
 };
