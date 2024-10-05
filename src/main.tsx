68f558ed6543dcb134a2d3b9271f4b0310c5f159
FahimFBA <fahimbinamin@gmail.com>
2024-10-05 21:36:56 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { supabase } from "./config/supabase.config.ts";
+import { persistedStore, store } from "./store/index.ts";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <SessionContextProvider supabaseClient={supabase}>
+      <PersistGate loading={null} persistor={persistedStore}>
+        <App />
+        <Toaster
+          position="bottom-right"
+          richColors
+          expand={false}
+          toastOptions={{}}
+        />
+      </PersistGate>
+    </SessionContextProvider>
+  </Provider>,
 );
