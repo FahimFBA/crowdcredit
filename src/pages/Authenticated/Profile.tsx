3f612c3d79a6157d75042f82a74c7114e2d055c4
AbdulHaqueRahiD <149531767+AbdulHaqueRahiD@users.noreply.github.com>
2024-10-05 21:30:50 +0600

    update profile

--- src/pages/Authenticated/Profile.tsx
+++ src/pages/Authenticated/Profile.tsx
@@ -1,5 +1,100 @@
+import {
+  AddressDetails,
+  PersonalDetails,
+  ProfessionCard,
+  UniversityDetails,
+} from "@/components/Cards/ProfileCards";
 import { withTemplate } from "../_Templates";
+import {
+  IAddressData,
+  IProfessionData,
+  IProfileData,
+  IUniversityData,
+} from "@/types/interface";
+import {
+  useGetUserProfileDetailsQuery,
+  useUpdateUserProfileDetailsMutation,
+} from "@/store";
+import { LoadingUI } from "@/components/Skeleton";
 
 export const Profile = withTemplate(() => {
-  return <div>Profile</div>;
+  const { data, isError, isFetching, isLoading } =
+    useGetUserProfileDetailsQuery();
+
+  const [updateUserProfileDetails, { isLoading: isUpdating }] =
+    useUpdateUserProfileDetailsMutation();
+
+  const handleUpdateProfile = async (data: IProfileData) => {
+    await updateUserProfileDetails({
+      data,
+    });
+  };
+
+  const handleUpdateUniversity = async (universityData: IUniversityData) => {
+    await updateUserProfileDetails({
+      data: {
+        ...(data as IProfileData),
+        university: universityData,
+      },
+    });
+  };
+
+  const handleUpdateAddress = async (addressData: IAddressData) => {
+    await updateUserProfileDetails({
+      data: {
+        ...(data as IProfileData),
+        address: addressData,
+      },
+    });
+  };
+
+  const handleUpdateProfession = async (addressData: IProfessionData) => {
+    await updateUserProfileDetails({
+      data: {
+        ...(data as IProfileData),
+        profession: addressData,
+      },
+    });
+  };
+
+  if (isLoading || isFetching) {
+    return <LoadingUI />;
+  }
+  if (isError) {
+    return <div>error</div>;
+  }
+
+  const university = data?.university as IUniversityData;
+  const personalDetails = data as IProfileData;
+  const userAddress = data?.address as IAddressData;
+  const professionData = data?.profession as IProfessionData;
+
+  return (
+    <div className="grid grid-cols-2 gap-3">
+      <PersonalDetails
+        title="Personal Details"
+        data={personalDetails}
+        handleUpdate={handleUpdateProfile}
+        isLoading={isUpdating}
+      />
+      <UniversityDetails
+        title="University Details"
+        data={university}
+        handleUpdate={handleUpdateUniversity}
+        isLoading={isUpdating}
+      />
+      <AddressDetails
+        title="Address"
+        data={userAddress}
+        handleUpdate={handleUpdateAddress}
+        isLoading={isUpdating}
+      />
+      <ProfessionCard
+        title="Profession"
+        data={professionData}
+        handleUpdate={handleUpdateProfession}
+        isLoading={isUpdating}
+      />
+    </div>
+  );
 });
