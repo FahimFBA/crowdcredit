4ed2a68bd8aadc9d1472fe4919637fdbf629b7e3
FahimFBA <fahimbinamin@gmail.com>
2024-10-05 22:49:01 +0600

    Update Home.tsx
    remove: Home text in homepage

--- src/pages/GeneralPages/Home.tsx
+++ src/pages/GeneralPages/Home.tsx
@@ -3,5 +3,5 @@ import { withTemplate } from "../_Templates";
 import { containerDefinition } from "@/_Variables";
 
 export const Home = withTemplate(() => {
-  return <div className={cn(containerDefinition, "mt-10 pt-10")}>Home</div>;
+  return <div className={cn(containerDefinition, "mt-10 pt-10")}></div>;
 });
