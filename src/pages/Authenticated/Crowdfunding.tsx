2067246e9a8bf3e906e977024cff35d545e729ef
FahimFBA <fahimbinamin@gmail.com>
2024-10-05 23:24:19 +0600

    Update Crowdfunding.tsx
    fix: label

--- src/pages/Authenticated/Crowdfunding.tsx
+++ src/pages/Authenticated/Crowdfunding.tsx
@@ -107,7 +107,7 @@ export const Crowdfunding = withTemplate(({ reduxState }) => {
   return (
     <div className="mb-10">
       <div className="flex flex-col items-center">
-        <h1 className="text-xl mb-4 font-bold capitalize">Crowd Funding</h1>
+        <h1 className="text-xl mb-4 font-bold capitalize">Crowdfunding</h1>
         <CrowdFundingSheet
           title="Create Crowd Funding Project"
           description="Please fill in the details to create a new crowd funding project"
