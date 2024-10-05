eab55a6b20637719f9150be827beff88405f14fc
AbdulHaqueRahiD <149531767+AbdulHaqueRahiD@users.noreply.github.com>
2024-10-05 21:30:13 +0600

    update index with new

--- src/pages/Authenticated/index.ts
+++ src/pages/Authenticated/index.ts
@@ -1,5 +1,7 @@
 export * from "./Dashboard";
 export * from "./Profile";
+export * from "./ProfileDetails";
 export * from "./Settings";
 export * from "./Crowdfunding";
 export * from "./CrowdfundingDetails";
+export * from "./LoanPostDetails";
