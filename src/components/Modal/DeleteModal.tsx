0f9a0f6978582aedb9e1bb7177ec3bc709c990ce
Israt Jahan Khan <isratjahankhanijk@gmail.com>
2024-10-05 21:09:25 +0600

    update deletemodal

--- src/components/Modal/DeleteModal.tsx
+++ src/components/Modal/DeleteModal.tsx
@@ -1,43 +1,42 @@
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog";
-  import { IDeleteEventModalProps } from "@/types/interface";
-  import { MouseEventHandler } from "react";
-  
-  export const DeleteModal = ({
-    title,
-    icon,
-    description,
-    onConfirm,
-  }: IDeleteEventModalProps) => {
-    return (
-      <AlertDialog>
-        <AlertDialogTrigger asChild>{icon}</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>{title}</AlertDialogTitle>
-            <AlertDialogDescription>{description}</AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction
-              onClick={
-                onConfirm as unknown as MouseEventHandler<HTMLButtonElement>
-              }
-            >
-              Continue
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    );
-  };
-  
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { IDeleteEventModalProps } from "@/types/interface";
+import { MouseEventHandler } from "react";
+
+export const DeleteModal = ({
+  title,
+  icon,
+  description,
+  onConfirm,
+}: IDeleteEventModalProps) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{icon}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={
+              onConfirm as unknown as MouseEventHandler<HTMLButtonElement>
+            }
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
